83c243b0acf85c9d48eaab9c02d3e29e25b593eb
IamLee99 <99619461+IamLee99@users.noreply.github.com>
2022-03-01 20:44:06 -0800

    Update BOB_the_Mannequin.py

--- BOB_the_Mannequin.py
+++ BOB_the_Mannequin.py
@@ -1,232 +1,89 @@
 '''
-"BOB_the_Mannequin" creates a Mannequin with movable limbs but stationary torso and head made out of
-                    multiple turtle.Turtle to mimic real-life mannequin statue.
-                    BOB_the_Mannequin.py itself creates Background, Bob's Head and torso,
-                    initial Limbs' coordinates, and limb rotating function and key binds based on
-                    functions given by Chassis.py. By doing this, major components of Mannequin comes
-                    from Chassis so the BOB_the_Mannequin.py be clean and simple.
-                    More in depth explanation will be in Chassis.py
+    "Chassis.py" is critical function ( can be considered as internal frame )
+    that provides "BOB_the_Mannequin.py" pre-colored & shaped limbs,
+    Coordinates of point of connection ( joint ) that continuously rotates for lower limbs, and
+    function that rotates by point of connection than turtle's coordinate by using Math and setx/y.
 '''
 
-# built-in lib
+
 import turtle
-# self-created Chassis.py needed to create Mannequin and locate coordinates of limbs' attaching points
-import Chassis
-
-# Creating Background for Bob to be created
-win = turtle.Screen()
-win.title("Mannequinn")
-win.bgcolor("skyblue")
-win.setup(width=500, height=500)
-# To stop Screen from continuously update while Bob being created, so it won't look messy
-win.tracer(0)
+import math
 
 
-# WARNING : BOB IS MIRRORED TO YOU // HIS LEFT IS WRITTEN AS RIGHT IN CODINGS
-# Creating BOB
-
-# need to create class for head.
-bob_head = Chassis.Limbs(0, 90)
-bob_head.shape("circle")
-bob_head.shapesize(stretch_wid=2.1, stretch_len=2.1)
-
-# Creating Bob's torso and limbs by using Chassis.Body or Chassis.Limbs which stores basic shape, size
-# and colors for less redundancy
-bob_body = Chassis.Body(0, 30)
-
-bob_arm_r_u = Chassis.Limbs(45, 70)
-
-bob_arm_l_u = Chassis.Limbs(-45, 70)
-
-bob_leg_r_u = Chassis.Limbs(20.2, -49.54)
-bob_leg_r_u.right(80)
-
-bob_leg_l_u = Chassis.Limbs(-20.2, -49.54)
-bob_leg_l_u.left(80)
-
-# lower limbs had to be generated with coordinates given by joint_xy() to accommodate slight
-# coordinate differences made by rotations
-# Lower Arms
-bob_arm_r_low = Chassis.Limbs(Chassis.joint_xy(bob_arm_r_u, 60).get('xcor'),
-                              Chassis.joint_xy(bob_arm_r_u, 60).get('ycor'))
-
-bob_arm_l_low = Chassis.Limbs(Chassis.joint_xy(bob_arm_l_u, -60).get('xcor'),
-                              Chassis.joint_xy(bob_arm_l_u, -60).get('ycor'))
-
-# Lower Legs
-bob_leg_r_low = Chassis.Limbs(Chassis.joint_xy(bob_leg_r_u, 60).get('xcor'),
-                              Chassis.joint_xy(bob_leg_r_u, 60).get('ycor'))
-bob_leg_r_low.right(80)
-
-bob_leg_l_low = Chassis.Limbs(Chassis.joint_xy(bob_leg_l_u, -60).get('xcor'),
-                              Chassis.joint_xy(bob_leg_l_u, -60).get('ycor'))
-bob_leg_l_low.left(80)
-
-bob_full = (bob_head, bob_body, bob_arm_r_u, bob_arm_r_low, bob_arm_l_u,
-            bob_arm_l_low, bob_leg_r_u, bob_leg_r_low, bob_leg_l_u, bob_leg_l_low)
-
-# Below functions use rotate_limb method from Chassis to not only rotate, but also
-# slightly change coordinates based on point of connection ( Joint ) given
-# Upper Limb Functions
-def right_arm_up():
-    bob_arm_r_u.rotate_limb('l', bob_body.body_joint(15, 40, 'r').get('xcor'),
-                            bob_body.body_joint(15, 40, 'r').get('ycor'), 30, 0)
-    right_low_arm_up()
+# Main class that gives basic shape, color, and delete pen tracing for Head, Body, and Limbs
+class Chassis(turtle.Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("square")
+        self.color('black', "white")
+        self.penup()
 
 
-def right_arm_down():
-    bob_arm_r_u.rotate_limb('r', bob_body.body_joint(15, 40, 'r').get('xcor'),
-                            bob_body.body_joint(15, 40, 'r').get('ycor'), 30, 0)
-    right_low_arm_down()
+# A body for Bob, stationary and more of a placeholder for limbs
+class Body(Chassis):
+    def __init__(self, x, y):
+        super().__init__()
+        self.shapesize(stretch_wid=5, stretch_len=1.5)
+        self.color('white', "grey")
+        self.setx(x)
+        self.sety(y)
+
+    def body_joint(self, x, y, direct):
+        dist = math.sqrt((x**2 + y**2))
+        if direct == 'r':
+            return {'xcor': self.xcor() + dist * (math.cos(math.atan(y / x) + math.radians(self.heading()))),
+                    'ycor': self.ycor() + dist * (math.sin(math.atan(y / x) + math.radians(self.heading())))}
+
+        elif direct == 'l':
+            return {'xcor': self.xcor() + dist * (math.cos(math.atan(y / x) + math.radians(self.heading()+180))),
+                    'ycor': self.ycor() + dist * (math.sin(math.atan(y / x) + math.radians(self.heading()+180)))}
 
 
-def left_arm_up():
-    bob_arm_l_u.rotate_limb('r', bob_body.body_joint(-15, 40, 'l').get('xcor'),
-                            bob_body.body_joint(-15, 40, 'l').get('ycor'), 30, 180)
-    left_low_arm_up()
+# Class Limbs will accommodate both Math Function and coordinates given to
+# rotate itself while keep in contact with either upper limbs or body to mimic the joint of Mannequin
+class Limbs(Chassis):
+    def __init__(self, x, y):
+        super().__init__()
+        self.shapesize(stretch_wid=0.7, stretch_len=3)
+        self.setx(x)
+        self.sety(y)
+
+# rotate_limb is probably the most crucial function in this project as this will make limbs
+# rotate and slightly move its coordinates to keep in contact with point of connection (aka Joints).
+# change of coordinate is calculated by coordinates of joint (given),
+# distance from joint to center of limb (a coordinate that setx/y follows), and a coordinate of
+# center of limb given by trigonometry based on distance and headings.
+    def rotate_limb(self, direct, x, y, dist, heading):
+        if direct == 'r':
+            self.right(9)
+        elif direct == 'l':
+            self.left(9)
+        self.setx(x + dist * (math.cos(math.radians(self.heading()+heading))))
+        self.sety(y + dist * (math.sin(math.radians(self.heading()+heading))))
 
 
-def left_arm_down():
-    bob_arm_l_u.rotate_limb('l', bob_body.body_joint(-15, 40, 'l').get('xcor'),
-                            bob_body.body_joint(-15, 40, 'l').get('ycor'), 30, 180)
-    left_low_arm_down()
+# joint_xy returns coordinates of point of connection (joint) between Upper and Lower limbs via
+# trigonometry because this joint is not stationary as Upper limb also rotates and
+# changes self coordinates. internal of function is similar to setx/y in rotate_limb_() because
+# it's calculating coordinate while accommodating distance and rotation,
+# but this time it's not changing any set of coordinates
+def joint_xy(limb, dist):
+    return {'xcor': limb.xcor() + dist * (math.cos(math.radians(limb.heading()))),
+            'ycor': limb.ycor() + dist * (math.sin(math.radians(limb.heading())))}
 
 
-def right_leg_up():
-    bob_leg_r_u.rotate_limb('l', bob_body.body_joint(15, -50, 'r').get('xcor'),
-                            bob_body.body_joint(15, -50, 'r').get('ycor'), 30, 0)
-    right_low_leg_up()
+def move_all(bob, direct):
+    x = 0
+    y = 0
+    if direct == 'r':
+        x = 10
+    elif direct == 'l':
+        x = -10
+    elif direct == 'up':
+        y = 10
+    elif direct == 'down':
+        y = -10
 
-
-def right_leg_down():
-    bob_leg_r_u.rotate_limb('r', bob_body.body_joint(15, -50, 'r').get('xcor'),
-                            bob_body.body_joint(15, -50, 'r').get('ycor'), 30, 0)
-    right_low_leg_down()
-
-
-def left_leg_up():
-    bob_leg_l_u.rotate_limb('r', bob_body.body_joint(-15, -50, 'l').get('xcor'),
-                            bob_body.body_joint(-15, -50, 'l').get('ycor'), 30, 180)
-    left_low_leg_up()
-
-
-def left_leg_down():
-    bob_leg_l_u.rotate_limb('l', bob_body.body_joint(-15, -50, 'l').get('xcor'),
-                            bob_body.body_joint(-15, -50, 'l').get('ycor'), 30, 180)
-    left_low_leg_down()
-
-
-# Lower Limb Functions, for lower limbs, their given point of connection changes depends on
-# Upper Limb because they also rotate. By giving point of connection based off of
-# Upper Limbs' coordinate rather than stationary point,
-# Lower Limb will be able to stay in point with Lower Limb and rotate.
-# Right Low Arm
-def right_low_arm_up():
-    bob_arm_r_low.rotate_limb('l', Chassis.joint_xy(bob_arm_r_u, 30).get('xcor'),
-                              Chassis.joint_xy(bob_arm_r_u, 30).get('ycor'), 30, 0)
-
-def right_low_arm_down():
-    bob_arm_r_low.rotate_limb('r', Chassis.joint_xy(bob_arm_r_u, 30).get('xcor'),
-                              Chassis.joint_xy(bob_arm_r_u, 30).get('ycor'), 30, 0)
-
-
-# Left Low Arm
-def left_low_arm_up():
-    bob_arm_l_low.rotate_limb('r', Chassis.joint_xy(bob_arm_l_u, -30).get('xcor'),
-                              Chassis.joint_xy(bob_arm_l_u, -30).get('ycor'), 30, 180)
-
-def left_low_arm_down():
-    bob_arm_l_low.rotate_limb('l', Chassis.joint_xy(bob_arm_l_u, -30).get('xcor'),
-                              Chassis.joint_xy(bob_arm_l_u, -30).get('ycor'), 30, 180)
-
-
-# Right Low Leg
-def right_low_leg_up():
-    bob_leg_r_low.rotate_limb('l', Chassis.joint_xy(bob_leg_r_u, 30).get('xcor'),
-                              Chassis.joint_xy(bob_leg_r_u, 30).get('ycor'), 30, 0)
-
-def right_low_leg_down():
-    bob_leg_r_low.rotate_limb('r', Chassis.joint_xy(bob_leg_r_u, 30).get('xcor'),
-                              Chassis.joint_xy(bob_leg_r_u, 30).get('ycor'), 30, 0)
-
-
-# Left Low Leg
-def left_low_leg_up():
-    bob_leg_l_low.rotate_limb('r', Chassis.joint_xy(bob_leg_l_u, -30).get('xcor'),
-                              Chassis.joint_xy(bob_leg_l_u, -30).get('ycor'), 30, 180)
-
-def left_low_leg_down():
-    bob_leg_l_low.rotate_limb('l', Chassis.joint_xy(bob_leg_l_u, -30).get('xcor'),
-                              Chassis.joint_xy(bob_leg_l_u, -30).get('ycor'), 30, 180)
-
-
-# new code testing
-def bob_to_right():
-    if bob_body.xcor() < 180:
-        Chassis.move_all(bob_full, 'r')
-
-def bob_to_left():
-    if bob_body.xcor() > -180:
-        Chassis.move_all(bob_full, 'l')
-
-def bob_to_up():
-    if bob_body.ycor() < 150:
-        Chassis.move_all(bob_full, 'up')
-
-def bob_to_down():
-    if bob_body.ycor() > -130:
-        Chassis.move_all(bob_full, 'down')
-
-# testing whole body rot
-def bob_all_rotate_right():
-    bob_body.right(9)
-    bob_head.rotate_limb('r', bob_body.xcor(), bob_body.ycor(), 60, 90)
-    right_arm_down(), right_low_arm_down(), left_arm_up(), left_low_arm_up(),
-    right_leg_down(), right_low_leg_down(), left_leg_up(), left_low_leg_up()
-
-def bob_all_rotate_left():
-    bob_body.left(9)
-    bob_head.rotate_limb('l', bob_body.xcor(), bob_body.ycor(), 60, 90)
-    right_arm_up(), right_low_arm_up(), left_arm_down(), left_low_arm_down(),
-    right_leg_up(), right_low_leg_up(), left_leg_down(), left_low_leg_down()
-
-
-# Restarting Tracer so that movement of Limbs ( turtle ) will be seen again in screen
-win.tracer(1)
-# To authorize key binding to be recognized by screen
-win.listen()
-
-# Right Arm, these keys will give rotation orders to right arm system.
-win.onkeypress(right_arm_up, 'e')
-win.onkeypress(right_arm_down, 'd')
-win.onkeypress(right_low_arm_up, 'r')
-win.onkeypress(right_low_arm_down, 'f')
-
-# Left Arm, these keys will give rotation orders to left arm system.
-win.onkeypress(left_arm_up, 'q')
-win.onkeypress(left_arm_down, 'a')
-win.onkeypress(left_low_arm_up, 'w')
-win.onkeypress(left_low_arm_down, 's')
-
-# Right Leg, these keys will give rotation orders to right leg system.
-win.onkeypress(right_leg_up, 'i')
-win.onkeypress(right_leg_down, 'k')
-win.onkeypress(right_low_leg_up, 'o')
-win.onkeypress(right_low_leg_down, 'l')
-
-# Left Leg, these keys will give rotation orders to left leg system.
-win.onkeypress(left_leg_up, 'y')
-win.onkeypress(left_leg_down, 'h')
-win.onkeypress(left_low_leg_up, 'u')
-win.onkeypress(left_low_leg_down, 'j')
-
-win.onkeypress(bob_to_right, 'Right')
-win.onkeypress(bob_to_left, 'Left')
-win.onkeypress(bob_to_up, 'Up')
-win.onkeypress(bob_to_down, 'Down')
-
-
-win.onkeypress(bob_all_rotate_right, 'x')
-win.onkeypress(bob_all_rotate_left, 'z')
-while True:
-    win.update()
+    for limbs in bob:
+        limbs.setx(limbs.xcor()+x)
+        limbs.sety(limbs.ycor()+y)
